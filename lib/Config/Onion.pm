456b4ca3ba2bc04e755396eae2ac793f386de327
Dave Sherohman <Dave.Sherohman@ub.lu.se>
2012-12-03 11:47:07 +0100

    Stubbed in everything for a proper dzil distro

---
+++ lib/Config/Onion.pm
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+package Config::Onion;
+
+1;
+
+__END__
+
+# ABSTRACT: Layered configuration, because configs are like ogres
+
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+use_ok 'Config::Onion';
+
+done_testing;
+
